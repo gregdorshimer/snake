4c4d4a3c364f285d7c5525cffe0833fff118c2de
Greg Dorshimer <greg.dorshimer@protonmail.com>
2024-10-15 17:18:46 -0400

    added junit dependency

---
+++ src/BallTests.java
@@ -0,0 +1,24 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class BallTests {
+
+    @Test
+    void ballTest() {
+        // assertEquals(2, calculator.add(1, 1));
+        // TODO
+    }
+
+    @Test
+    void makeAppleTest() {
+        // TODO
+    }
+
+    @Test
+    void moveBallTest() {
+        // TODO
+    }
+
+}
